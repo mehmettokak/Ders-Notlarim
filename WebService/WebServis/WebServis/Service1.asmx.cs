b8f85da53cea3de22141a64a5e676ded824c3c99
mehmettokak <tokakmehmet@gmail.com>
2016-07-01 19:06:17 +0300

    WebService
    Notlar,Ekran Goruntuleri,Uygulama

---
+++ WebService/WebServis/WebServis/Service1.asmx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServis
+{
+    //uzantı asmx ise webservis projesi demektir.
+    //Web Serviste kullanılan standartlar :
+    // XML=>Veri alış verişlerinde kullanılanılır.
+    // WSDL(WebServis Description Language) => Kullanıcılar webservis arayuz hakkında bilgi sunar.
+    // DİSCO (Discovery Protokol) Sunucuya açılmış paylaşımdaki servislerin organizasyonunu sağlar.(onu alıp oraya bunu alıp buraya gibi işlemlerde kullanıyor.)
+    // UDDI =>İnternet üzerinde açılmış ve uygulamalar tarafında kullanılabilecek web servis organizasyonunu saglar.
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+ 
+    public class Service1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]//Metodumuz Web serviste kullanılabilir demektir.Yazılmasa kullanılmaz.
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+         [WebMethod]//webserviste kullanılabilir yaptım.
+        public DataTable urungetir()
+        
+        {
+             //Haber databasesinden haber tablosunu cektim.Bunu serviste invok edersek xml cıktısı gozukur.
+            SqlConnection cnn = new SqlConnection("Server=BYGURO-PC;Database=Haber;User Id=sa;Password=123;");
+            cnn.Open();
+            SqlDataAdapter adp = new SqlDataAdapter("select * from Haber", cnn);
+            DataTable dt = new DataTable("Haber");
+            adp.Fill(dt);
+            return dt;
+        }
+    }
+}
---
+++ WebService/WebServis/testproje/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace testproje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //servis1soapclient=>servis1 Karsı taraftaki web seriv adı asmx uzantılı olan.Soapclient=>soap elemanına uygun bir tane istemci olustur.
+            Baglandigimizwebservis.Service1SoapClient mehmet = new Baglandigimizwebservis.Service1SoapClient();//ornek aldık.
+            //burada karsı tarafın olusturdugu metotları gorebiliyoruz.
+            dataGridView1.DataSource = mehmet.urungetir();//urun bilgilerini gridviev ımıza ekledik.
+            //Karsı taraftan urunleri alırken uzaktakı bilgisayar a baglanacak.baglandıgı bilg ip ,kadi,ksifre,hangi tablodan geldiğini bilmiyor.Sadece sonuc alıyor.
+            //  bilgi xml gelmesine karsın ben datnet uyg geliştirdiğim için bana geriye datatable verdiği için ben burda direkt datatable olarak alıyorum.Dolayısıyla benım xml ile ugraşmama gerek kalmıyor.bunu yapan serilazerdir.karsı tarftan gelen xml bılgısı serilazer edilerek data tableye donusuyor benım projemde.
+        }
+    }
+}
