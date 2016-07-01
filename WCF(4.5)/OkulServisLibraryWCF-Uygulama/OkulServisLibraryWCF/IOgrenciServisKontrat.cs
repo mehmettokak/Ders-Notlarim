2f627f6daf3ed838d94f366406be28255a377220
mehmettokak <tokakmehmet@gmail.com>
2016-07-01 19:01:29 +0300

    WCF
    WCF Notlarım,Ekran goruntuleri,Uygulama

---
+++ WCF(4.5)/OkulServisLibraryWCF-Uygulama/OkulServisLibraryWCF/IOgrenciServisKontrat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace OkulServisLibraryWCF
+{
+    [ServiceContract] //Kontratı dıs dunyaya açtık
+  public   interface IOgrenciServisKontrat //bu clası interface ile açtık.
+    {
+       
+        [OperationContract] //Dış dunyaya açık.
+        List<Ogrenci> OgrenciListele();//interface oldugu icin metot olarak tanımlamamız gerekir.
+
+        [OperationContract] //Dış dunyaya açık.
+        Ogrenci OgrenciGetir(int id);//id ye göre tek bir ogrenci  getiren metod.
+
+        [OperationContract] //Dış dunyaya açık.
+        void OgrenciSil(int id);//geriye deger donmeyen id ye gore ogrenci silen metod.
+
+        [OperationContract] //Dış dunyaya açık.
+        void OgrenciEkle(Ogrenci ogrenci);// Ögrenci tipinde ogrenci nesnesi alarak ogrenci ekleyebilir.
+
+        //OperationContract yazmadıgımız için dıs dunyaya kapalıdır.kımse erisemez.Serileştirilmeyecek
+        void OgrenciGuncelle(Ogrenci ogrenci);// Ögrenci tipinde ogrenci nesnesi alarak ogrenci güncelleyebilir.
+    }
+}
---
+++ WCF(4.5)/OkulServisLibraryWCF-Uygulama/OkulServisLibraryWCF/Ogrenci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OkulServisLibraryWCF
+{
+    //ögrenci clasınıd dış dunya ya sunmak için ,yani dısardaki kullanıcıların bizim oluşturduğumuz ögrenci class ına (nesnesine) ulaşabilmesi için datacontract attibut u ile süslememiz gerekiyor. 
+    [DataContract]
+   public class Ogrenci
+    {
+        [DataMember]
+        public int ogrenciId { get; set; }
+
+        [DataMember]
+        public string ogrenciAdi { get; set; }
+
+        [DataMember]
+        public string ogrenciSehri { get; set; }
+    }
+}
